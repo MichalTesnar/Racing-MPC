c8e141b60e3200f449725ccdcac8c5537786bb8d
Michal Tesnar <michal.tesnar007@gmail.com>
2022-12-23 10:27:46 +0100

    Initial commit

---
+++ generate_paths.m
@@ -0,0 +1,108 @@
+clear all; clc;
+close all; drawnow;
+
+keySet = ["tau", "width", "steps", "global_steps"];
+valueSet = [0.01 0.2 50 300];
+constant = containers.Map(keySet,valueSet);
+
+% generating track
+test_path_x = (1:800)/20;
+test_path_y = 0.2*sin(0.5*test_path_x);
+bound_upper = test_path_y + constant("width")/2;
+bound_lower = test_path_y - constant("width")/2;
+
+% starting position of the model
+start_x = 0;
+start_y = 0;
+start_theta = 0.02;
+
+% temporary variables to keep track of the progress
+global best; % best progress along the line
+best = 0;
+global best_v; % best velocity
+best_v = 0;
+global best_omega % best omega
+best_omega = 0;
+global best_x; % best path we have taken x
+best_x = double(zeros(constant("steps")));
+global best_y; % best path we have taken y
+best_y = double(zeros(constant("steps")));
+
+% storing the path we actually took
+global taken_x;
+taken_x = zeros(constant("global_steps"), 1);
+taken_x(1) = start_x;
+global taken_y;
+taken_y = zeros(constant("global_steps"), 1);
+taken_y(1) = start_y;
+global taken_omega;
+taken_omega = zeros(constant("global_steps"), 1);
+global taken_velocity;
+taken_velocity = zeros(constant("global_steps"), 1);
+taken_velocity(1) = 0;
+
+
+global last_index;
+last_index = 0;
+
+% main loop
+for a = 2:constant("global_steps")
+    for velocity = 5:15 % try velocities
+        for angle = -10:10 % try angles
+
+            % constraint the lateral acceleration
+            omega = angle/30;
+            lateral_acceleration = omega*velocity;
+
+
+            if lateral_acceleration < 4
+                % let the model move in that way
+                [path_x, path_y, ~] = unicycle_model(velocity, omega, start_x, start_y, start_theta, constant("steps"), constant("tau"));
+                
+
+                valid = path_checking(path_x, path_y, test_path_x, test_path_y, last_index, constant("width"));
+                if valid == true % if valid, take find closest point to the last point
+                    global best
+                    best = 0;
+                    [progress, distance] = find_closest_point_on_the_line([path_x(constant("steps")), path_y(constant("steps"))], test_path_x, test_path_y, last_index);
+                    if progress > best % update maximum
+                        best = progress;
+                        best_v = velocity;
+                        best_omega = omega;
+                        best_x = path_x;
+                        best_y = path_y;
+                    end
+                end
+            end
+        end
+    end
+    [path_x, path_y, path_theta] = unicycle_model(best_v, best_omega, start_x, start_y, start_theta, 2, constant("tau"));
+    start_x = path_x(2);
+    start_y = path_y(2);
+    start_theta = path_theta(2);
+    taken_x(a) = start_x;
+    taken_y(a) = start_y;
+    taken_omega(a) = best_omega;
+    taken_velocity(a) = best_v;
+    
+end
+
+% plotting all the curves
+figure
+
+% reference path to follow
+% plot(test_path_x, test_path_y, "o");
+% hold on
+
+% upper bound
+plot(test_path_x, bound_upper, 'ob', 'MarkerSize',1);
+hold on
+
+% lower bound
+plot(test_path_x, bound_lower, 'oy', 'MarkerSize',1);
+hold on
+
+% path the model took
+scatter(taken_x, taken_y, 200*abs(taken_omega)+0.000001, taken_velocity+1, 'filled');
+% fixating the axes
+axis([0 40 -1 1])
---
+++ unicycle_model.m
@@ -0,0 +1,18 @@
+
+
+function [positions_x, positions_y, positions_theta] = unicycle_model(velocity, omega, current_x, current_y, current_theta, k, tau)
+    % initiate arrays for future positions and angle
+    positions_x = double(zeros(k));
+    positions_y = double(zeros(k));
+    positions_theta = double(zeros(k));
+    % set first values of those arrays
+    positions_x(1) = current_x;
+    positions_y(1) = current_y;
+    positions_theta(1) = current_theta;
+    % iteratively compute the future behavior of the model
+    for i = 2:k
+        positions_x(i) = positions_x(i-1) + tau*velocity*cos(positions_theta(i-1));
+        positions_y(i) = positions_y(i-1) + tau*velocity*sin(positions_theta(i-1));
+        positions_theta(i) = positions_theta(i-1) + tau*omega;
+    end
+end
